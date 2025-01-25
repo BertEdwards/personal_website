8295d73b62a2e9f278c508a0abe2c80bd53d244e
Bert Edwards <bertrand.edwards20@imperial.ac.uk>
2025-01-25 10:19:07 +0000

    Nav bar progress

--- my-project/src/Components/header.jsx
+++ my-project/src/Components/header.jsx
@@ -1,5 +1,21 @@
 
+/**
+ * Components
+ */
+import { useState } from "react";
+
+
+/**
+ * Components
+ */
+
+import Navbar from "./navbar"
+
+
+
 const Header = () => {
+    const [navOpen, setNavOpen] = useState(true);
+    
     return (
         <header className="fixed top-0 left-0 w-full h-20 flex
         ietms-center z-40 bg-gradient-to-b from-zinc-900
@@ -21,19 +37,19 @@ const Header = () => {
                 <div className="relative md:justify-self-center">
                     <button
                     className="menu-btn md:hidden"
-                    onClick={null}
+                    onClick={() => setNavOpen((prev) => !prev)}
                     >
                         <span className="material-symbols-rounded">
                             menu
                         </span>
                     </button>
 
-                    Navbar
+                    <Navbar  navOpen={navOpen}/>
                 </div>
 
                 <a 
                 href="#contact"
-                className=""
+                className="btn btn-secondary max-md:hidden "
                 >
                     Contact Me
                 </a>
